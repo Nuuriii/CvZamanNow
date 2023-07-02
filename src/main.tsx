9c4803c145b85e066d3d1d04c375b4dc4130f089
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-02 17:36:13 +0700

    add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <LandingPage />,
+   },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+   <React.StrictMode>
+      <RouterProvider router={router} />
+   </React.StrictMode>
+);
