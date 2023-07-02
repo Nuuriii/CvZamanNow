ccf03baac9f5aaa6c48f187542c016e255b8d222
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-02 23:42:53 +0700

    fix responsive for mobile and Dekstop

--- src/components/Content2/Content2.tsx
+++ src/components/Content2/Content2.tsx
@@ -3,15 +3,15 @@ import ArrowRight from "/img/ArrowRight.svg";
 
 function Content2() {
    return (
-      <div className='flex flex-col items-center py-9 px-7 gap-y-9'>
-         <div>
-            <img src={Image} alt='' />
+      <div className='flex flex-col items-center justify-center sm:flex-row sm:gap-8 py-9 px-7 gap-y-9 lg:py-10'>
+         <div className='flex justify-center lg:w-1/2'>
+            <img className='lg:h-96 lg:w-96' src={Image} alt='' />
          </div>
-         <div className='flex flex-col justify-center gap-y-6'>
-            <h1 className='text-4xl font-semibold'>
+         <div className='flex flex-col justify-center lg:w-1/2 gap-y-6 '>
+            <h1 className='text-4xl font-semibold lg:text-5xl '>
                Ciptakan peluang diterima di perusahaan impian
             </h1>
-            <p>
+            <p className='lg:text-lg'>
                Buat perbedaan di pasar kerja dengan CV terbaik dari CV Zaman
                Now.
             </p>
