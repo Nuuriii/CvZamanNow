95a533168ee85e162abe3c88c1ff15fa55f650a4
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-04 17:55:35 +0700

    add content in ImproveCareer

--- src/components/ImproveCareer/ImproveCareer.tsx
+++ src/components/ImproveCareer/ImproveCareer.tsx
@@ -1,3 +1,63 @@
 export default function ImproveCareer() {
-   return <div>ImproveCareer</div>;
+   return (
+      <div className='pt-10 px-7'>
+         <div>
+            <div className='py-5'>
+               <h1 className='text-3xl font-semibold'>
+                  Tingkatkan kariermu ke level selanjutnya
+               </h1>
+            </div>
+            <div className='flex flex-col gap-4'>
+               <div className='flex justify-center gap-3'>
+                  <div className='flex items-start gap-3'>
+                     <span className='flex items-center justify-center w-10 h-10 font-bold text-white bg-green-900 rounded-full'>
+                        01
+                     </span>
+                  </div>
+                  <div className='flex flex-col items-start pt-1 gap-y-1'>
+                     <h3 className='text-lg font-semibold text-green-950'>
+                        Banyak pilihan template
+                     </h3>
+                     <p>
+                        Menyediakan berbagai template CV modern dan profesional.
+                     </p>
+                  </div>
+               </div>
+               <div className='flex justify-center gap-3'>
+                  <div className='flex items-start gap-3'>
+                     <span className='flex items-center justify-center w-10 h-10 font-bold text-white bg-green-900 rounded-full'>
+                        02
+                     </span>
+                  </div>
+                  <div className='flex flex-col items-start pt-1 gap-y-1'>
+                     <h3 className='text-lg font-semibold text-green-950'>
+                        Profil yang selalu up-to-date
+                     </h3>
+                     <p>
+                        Membantu pengguna untuk mengatur dan memperbarui
+                        informasi CV secara mudah.
+                     </p>
+                  </div>
+               </div>
+               <div className='flex justify-center gap-3'>
+                  <div className='flex items-start gap-3'>
+                     <span className='flex items-center justify-center w-10 h-10 font-bold text-white bg-green-900 rounded-full'>
+                        03
+                     </span>
+                  </div>
+                  <div className='flex flex-col items-start pt-1 gap-y-1'>
+                     <h3 className='text-lg font-semibold text-green-950'>
+                        Mudah Digunakan
+                     </h3>
+                     <p>
+                        Mempermudah pengguna untuk membuat CV yang menarik dan
+                        profesional.
+                     </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div></div>
+      </div>
+   );
 }
