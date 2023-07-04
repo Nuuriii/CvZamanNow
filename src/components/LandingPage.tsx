982f99313c5553d625d6632c673a552eb98c602f
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-04 07:18:47 +0700

    change name for Component

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,14 +1,14 @@
 import Navbar from "./Navbar/Navbar";
-import Content1 from "./Content1/Content1";
-import Content2 from "./Opportunity/Opportunity";
+import GettingStarted from "./Content1/Content1";
+import Opportunity from "./Opportunity/Opportunity";
 
 export default function LandingPage() {
    return (
       <div>
          <div className='lg:h-screen'>
             <Navbar />
-            <Content1 />
-            <Content2 />
+            <GettingStarted />
+            <Opportunity />
          </div>
       </div>
    );
