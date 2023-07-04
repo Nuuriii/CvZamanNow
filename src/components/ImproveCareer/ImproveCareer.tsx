db3334e7f9bb02c1d3508af625fabb68b1dd1f6e
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-04 07:28:48 +0700

    create ImproveCareer Component

---
+++ src/components/ImproveCareer/ImproveCareer.tsx
@@ -0,0 +1,12 @@
+import UpImage from "./img/Up.svg";
+
+export default function ImproveCareer() {
+   return (
+      <div>
+         <div>
+            <img src={UpImage} alt='' />
+         </div>
+         <div></div>
+      </div>
+   );
+}
